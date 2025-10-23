7863d1a1d7f57386c9b60c9fe3601ff626f933a6
priyanka-bidnal <bidnalpriyanka@gmail.com>
2025-10-23 20:39:44 +0530

    Update cal.py

--- cal.py
+++ cal.py
@@ -1,12 +1,18 @@
-def add(a, b):
-    return a + b
-
-def subtract(a, b):
-    return a - b
-
-if __name__ == "__main__":
-    x = float(input("Enter first number: "))
-    y = float(input("Enter second number: "))
+num1=float(input("Enter first number:"))
+num2=float(input("Enter second number:))
+operator=input("Enter operator:"))
+if operator=='+':
+    result=num1+num2
+elif operator=='-':
+    result=num1-num2
+elif operator=='*':
+    result=num1*num2
+elif operator=='/':
+    if num2!=0:
+        result=num1/num2  
+    else:
+        result="Error!"
+else:
+    result="Invalid operation"
+print("Result:",result)    
     
-    print("Addition:", add(x, y))
-    print("Subtraction:", subtract(x, y))
